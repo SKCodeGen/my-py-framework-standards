709a94c4800e3c138a880743045d017b062cb7c5
Suvarna Kukkapalli <suvarna.kukkapalli@gmail.com>
2021-09-12 12:46:41 -0500

    init files

---
+++ makefile
@@ -0,0 +1,7 @@
+install:
+
+build:
+
+test:
+
+publish:
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+
+setup_requires = ['setuptools']
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('VERSION') as f:
+    version = f.read().strip('\n')
+
+
+setup(
+    name='my-py-packaging',
+    description='Python framework and Standards',
+    long_description=readme,
+    version=version,
+    author='Suvarna K',
+    author_email='abc@gmail.com',
+    url='https://github.com/SKCodeGen',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'PyYAML~=5.4',
+        'pydantic',
+        'pandas',
+        'numpy',
+        'attrs',
+        'flake8',
+        'black',
+    ],
+    setup_requires=setup_requires,  # This should not be required.  There is a warning on pip documentation for not using.
+)
+
