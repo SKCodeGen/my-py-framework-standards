a512197ad7c6264770b1ddfca2769ee4cb1fdab5
Suvarna Kukkapalli <suvarna.kukkapalli@gmail.com>
2021-09-27 16:41:08 -0500

    rename

--- my-py-framework-standards/clients/listen_to_client.py
+++ my-py-framework-standards/clients/listen_to_client.py
@@ -1,9 +1,18 @@
+from typing import List
 from my_py_framework_standards.
 
 
 class ListenToClient(BaseClient):
     def __init__(self, client_name: str):
         self.client_name = client_name
+
+
+    @staticmethod
+    def add_three(self, vars: List[]):
+        total: int = 0
+        for var in vars:
+            total = total + var
+        return total
 
 
 
--- tests/unit/fixtures/listen.py
+++ tests/unit/fixtures/listen.py
@@ -3,3 +3,8 @@ import pytest
 
 @pytest.fixture
 def listean_to_client():
+    vars = [3, 8, 9]
+    return vars
+
+
+
